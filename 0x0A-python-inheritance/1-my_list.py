44e457082f7ddd690ee8fb75d02c19c39259ed47
Sofiag8 <1943@holbertonschool.com>
2020-10-06 23:21:08 +0000

    fic docstring perhaps

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,7 +5,5 @@
 class MyList(list):
     """ class my list"""
     def print_sorted(self):
-        """ public instance method definition
-        prints a sorted list (ascending sort)
-        """
+        """ prints a sorted list (ascending sort) """
         print(sorted(self))
