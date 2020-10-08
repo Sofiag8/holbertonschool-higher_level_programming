867e7c3ef422612bc498ef2c0216aeb5cbb98367
Sofiag8 <1943@holbertonschool.com>
2020-10-08 00:04:52 +0000

    list and str

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -18,10 +18,11 @@ class Student:
     def to_json(self, attrs=None):
         """ public method that retrieves a dictionary representation
         of a student instance """
-        if isinstance(attrs, list) and ([isinstance(n, list) for n in attrs]):
+        if isinstance(attrs, list) and ([isinstance(n, str)] for n in attrs):
             new_dictionary = {}
             for words in attrs:
                 if words in self.__dict__:
                     new_dictionary[words] = self.__dict__[words]
             return new_dictionary
+
         return self.__dict__
