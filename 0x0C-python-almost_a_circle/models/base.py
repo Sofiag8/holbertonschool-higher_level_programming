f71831d7af42925cf3085f6680b8f701ed2bcc91
Sofiag8 <1943@holbertonschool.com>
2020-10-13 19:01:14 +0000

    condition

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,4 +18,6 @@ class Base:
 
     def to_json_string(list_dictionaries):
         """ to JSON definition """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
         return json.dumps(list_dictionaries)
