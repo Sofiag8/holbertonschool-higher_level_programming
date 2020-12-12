cd5c4dbe9cd883c3bda3a50c68a760a4df974959
Sofiag8 <1943@holbertonschool.com>
2020-12-12 03:09:59 +0000

    Listing only the states names starting with N, ASC order id.state struct

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" script that lists all states with a name starting with upper N """
+import MySQLdb
+from sys import argv
+
+db = MySQLdb.connect(host="localhost", port=3306,
+                     user=argv[1], passwd=argv[2],
+                     db=argv[3], charset="utf8")
+cur = db.cursor()
+rows = cur.execute("SELECT * FROM states WHERE NAME like 'N%' ORDER BY id ASC")
+for i in range(rows):
+    result = cur.fetchone()
+    print(result)
