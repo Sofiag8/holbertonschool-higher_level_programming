c344a77fac21660e06dc4e4eaeee39481738e6e4
Sofiag8 <1943@holbertonschool.com>
2020-12-18 23:47:46 +0000

    taking in an url and showing its sizes

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# cript that takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response
+# cript that takes in a URL and shows its bytes size
 curl -s &1 | wc -c
