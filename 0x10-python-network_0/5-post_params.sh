9433505acf1e1d36d9cb4d7c47c74184d72ddd4d
Sofiag8 <1943@holbertonschool.com>
2020-12-19 00:58:32 +0000

    commit

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a POST request to the passed URL . POST use
-curl -s -X POST -d 'emacs=hr@holbertonschool.com&subject=I will always be here for PLD' $1
+curl -sX POST -d "emacs=hr@holbertonschool.com&subject=I will always be here for PLD" $1
