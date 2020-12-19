edb95654fdeaa098ecdf3af78a1c9efb68e75bb1
Sofiag8 <1943@holbertonschool.com>
2020-12-19 04:47:39 +0000

    pep8

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -15,8 +15,8 @@ def find_peak(list_of_integers):
         if list_of_integers[mid] > list_of_integers[mid + 1] and\
            list_of_integers[mid] > list_of_integers[mid - 1]:
             return list_of_integers[mid]
-        elif list_of_integers[mid] < list_of_integers[mid + 1] and\
-             list_of_integers[mid] > list_of_integers[mid - 1]:
+        if list_of_integers[mid] < list_of_integers[mid + 1] and\
+           list_of_integers[mid] > list_of_integers[mid - 1]:
             return list_of_integers[mid + 1]
         else:
             maxi = max(list_of_integers)
