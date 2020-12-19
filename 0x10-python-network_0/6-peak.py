6f16c1d060594cb19036e00782e393d8cd318b5c
Sofiag8 <1943@holbertonschool.com>
2020-12-19 03:11:29 +0000

    interview

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Peking method """
+
+
+def find_peak(list_of_integers):
+    """ find peak method in a list of unsorte integers
+    Args:
+    the integer list (list)
+    This algorithm must have the lowest complexity
+    """
+    left = 0
+    right = len(list_of_integers) - 1
+    while left < right:
+        mid = (left + right) // 2
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            right = mid
+            return right
+        else:
+            left = mid + 1
+            return left
