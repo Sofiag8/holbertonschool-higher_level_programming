2c863becaed16fd1593cb048991d30cfdb7ae204
Sofiag8 <1943@holbertonschool.com>
2020-12-19 04:56:40 +0000

    empty or just one

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -10,6 +10,8 @@ def find_peak(list_of_integers):
     """
     left = 0
     right = len(list_of_integers) - 1
+    if list_of_integers == None or len(list_of_integers) == 1:
+        return None
     while left < right:
         mid = (left + right) // 2
         if list_of_integers[mid] > list_of_integers[mid + 1] and\
