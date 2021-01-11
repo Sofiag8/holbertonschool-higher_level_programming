f51077e2a00144de7dac8c348dc80c8d6b334b0c
Sofiag8 <1943@holbertonschool.com>
2021-01-11 21:32:06 +0000

    error code, not Request or var

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -7,9 +7,8 @@ import sys
 
 if __name__ == "__main__":
 
-    url = urllib.request.Request(sys.argv[1])
     try:
-        with urllib.request.urlopen(url) as response:
+        with urllib.request.urlopen(sys.argv[1]) as response:
             print(response.read().decode('utf-8'))
     except urllib.error.HTTPError as e:
         print('Error code: ', e.code)
