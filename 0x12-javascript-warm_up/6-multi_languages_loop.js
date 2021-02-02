640c50cfbb91ad36fc786929cb93043e03420445
Sofiag8 <1943@holbertonschool.com>
2021-02-02 22:59:52 +0000

    looping with JS

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/* script that prints three lines with specific text,
+using just one consele.log and a loop */
+const phrases = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (const phrase of phrases) {
+  console.log(phrase);
+}
