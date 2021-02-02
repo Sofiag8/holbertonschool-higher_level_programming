84edb562579b51b4f122c031d419366531d3ccbe
Sofiag8 <1943@holbertonschool.com>
2021-02-02 19:57:45 +0000

    check if an argument was passed to a function with undefined

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// script that prints the first argument passed to it
+const argument = process.argv[2];
+if (argument === undefined) {
+  console.log('No argument');
+} else {
+  console.log(argument);
+}
