f1d95b26a0e7cfc73d3d6b87f95175a8bac47991
Sofiag8 <1943@holbertonschool.com>
2021-02-03 01:02:27 +0000

    remove comments

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 /* printing addition of two integers
 with a predetermine function */
-// constructor function
 const a = parseInt(process.argv[2]);
 const b = parseInt(process.argv[3]);
 function add (a, b) {
   return a + b;
 }
-// instantiation
 console.log(parseInt(add(a, b)));
