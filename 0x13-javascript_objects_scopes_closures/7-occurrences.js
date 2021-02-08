1d5458dbdc3f7639516ea3049f05e6e35eb4aefd
Sofiag8 <1943@holbertonschool.com>
2021-02-08 12:09:43 +0000

    occurrences, couple of comments

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
+  // a - accumulator, v - current value, ternary operator ? if true, else :
   const counter = list.reduce((a, v) => (v === searchElement ? a + 1 : a), 0);
   return counter;
 };
