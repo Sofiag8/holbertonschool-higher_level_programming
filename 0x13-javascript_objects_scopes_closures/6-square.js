062107a952d683f81adee2f4a8a5d1d00ef3dc51
Sofiag8 <1943@holbertonschool.com>
2021-02-08 11:09:53 +0000

    sqaure inherited from Square

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+
+const square = require('./5-square.js');
+
+class Square extends square {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.height));
+    }
+  }
+}
+module.exports = Square;
