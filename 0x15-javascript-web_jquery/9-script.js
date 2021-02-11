dc9cb127bf929284f8f5d40ec4c5fae254b3bc74
Sofiag8 <1943@holbertonschool.com>
2021-02-11 05:34:57 +0000

    never is too late

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+$(document).ready(function () {
+    $.get('https://fourtonfish.com/hellosalut/?lang=fr', function (frenchgreet) {
+        $('#hello').text(frenchgreet.hello);
+    });
+});
