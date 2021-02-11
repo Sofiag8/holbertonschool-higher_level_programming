6ebe766888bb426f50052c4e307b20c18fc6e6c3
Sofiag8 <1943@holbertonschool.com>
2021-02-11 05:21:02 +0000

    --. .-. . . - .. -. --. ... / ..-. .-. --- -- / ... --- -- . --- -. . / .-- .... --- / .-.. --- ...- . ... / - --- / - .- .-.. -.- / .-- .. - .... / -.-- --- ..-

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -2,8 +2,8 @@
 
 
 $.get('https://swapi-api.hbtn.io/api/films/?format=json', function (movies) {
-  for (const titles of movies.results) {
-    const titles = $('<li></li>').text(movies.title);
-    $('#list_movies').append(titles);
+  for (const title of movies.results) {
+    const allTitles = $('<li></li>').text(title.title);
+    $('#list_movies').append(allTitles);
   }
 });
